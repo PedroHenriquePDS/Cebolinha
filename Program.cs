7a6fb410abd94d4980446dd2e625cadff3faed48
Pedro Henrique <PedroHenrique053010@gmail.com>
2022-09-29 21:30:51 -0300

    Adiciona a solução ao exercicio Cebolinha

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string textoDigitada, textoDislalia;
+
+Console.Write("Digite uma frase: ");
+textoDigitada = Console.ReadLine()!;
+
+textoDislalia = textoDigitada
+    .Replace("r", "l")
+    .Replace("R", "L");
+
+Console.WriteLine(textoDislalia);
+
+
+
+
